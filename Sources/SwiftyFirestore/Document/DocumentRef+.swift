d6b335ddb66324b9579532409ce1ddfcb518ca26
Yusuke Hosonuma <tobi462@gmail.com>
2020-04-11 12:39:37 +0900

    feature: document exists

---
+++ Sources/SwiftyFirestore/Document/DocumentRef+.swift
@@ -0,0 +1,23 @@
+//
+//  DocumentRef+.swift
+//  SwiftyFirestore
+//
+//  Created by Yusuke Hosonuma on 2020/04/11.
+//  Copyright © 2020 Yusuke Hosonuma. All rights reserved.
+//
+
+extension DocumentRef {
+    func exists(completion: @escaping (Result<Bool, Error>) -> Void) {
+        ref.getDocument { snapshot, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                if let snapshot = snapshot {
+                    completion(.success(snapshot.exists))
+                } else {
+                    preconditionFailure("Expect to not reachable.")
+                }
+            }
+        }
+    }
+}
